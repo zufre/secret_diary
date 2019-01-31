0ef810a0fb627a3377360e536947c0e9d4da082a
Cédric David <cedric.david@btopenworld.com>
2019-01-31 16:26:04 +1000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+      <link href="https://fonts.googleapis.com/css?family=Dancing+Script:700" rel="stylesheet"> 
+      <style type="text/css">
+          
+      .container {
+      
+            text-align: center;
+            width: 600px;
+      }
+          
+      #homePageContainer {
+              
+            margin-top: 150px;
+          
+      }
+          
+          #containerLoggedInPage {
+              
+              margin-top: 60px;
+              
+          }
+          
+          
+      html { 
+	  
+	  }
+          
+	  body {    
+	  
+           background: -webkit-linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(background.jpg) no-repeat center center fixed; 
+		   
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+			
+          
+              
+              color: #FFF;
+              
+          }
+          
+          #logInForm {
+              
+              display:none;
+				width: 300px;
+			  margin: 0 auto;
+          }
+		  #signUpForm {
+			    width: 300px;
+			  margin: 0 auto;
+		  }
+		  #error {
+			      width: 300px;
+			  margin: 0 auto;
+		  }
+          h1 {
+			  font-family: 'Dancing Script', cursive;
+			  font-size: 100px;
+		  }
+          .toggleForms {
+              
+              font-weight: bold;
+              
+          }
+          
+          #diary {
+              
+              width: 76%;
+              height: 100%;
+			  margin: 0 auto;
+			  border: 10px double;
+              
+          }
+          
+      </style>
+  </head>
+  <body>
